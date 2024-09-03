23d187424cbe867e8c1c8c7e548fe795eb6e9a52
Felipe Takeshi <ftrangel07@gmail.com>
2024-09-03 10:13:27 -0300

    serialization and deserialization

--- login system/main.cpp
+++ login system/main.cpp
@@ -2,7 +2,8 @@
 #include <string>
 #include <fstream>
 #include <vector>
-
+std::string desiredName;
+std::string desiredPassword;
 class client{
 
 private:
@@ -67,28 +68,57 @@ void writeElement(std::fstream & stream, std::string filename, std::string numfi
         std::cerr << "Failed to open file: " << filename << std::endl;
         exit(1);
     }
-    stream.write(reinterpret_cast<char *> (&user), sizeof(client));
+    //serialization
+
+    //writing name
+    int usernameSize = user.getUsername().length();
+    stream.write(reinterpret_cast<char * >(&usernameSize), sizeof(int));
+
+    stream.write(reinterpret_cast<char *> (&desiredName), usernameSize);
+
+    //writing password
+    int passwordSize = user.getPassword().length();
+    stream.write(reinterpret_cast<char *>(&passwordSize), sizeof(int));
+
+    stream.write(reinterpret_cast<char *> (&desiredPassword), passwordSize);
     stream.close();
 }
 void ReadElements(std::fstream & stream, std::fstream & numstream, std::string filename, std::string numfile){
     int num = getCharacter(numstream, numfile);
-    std::vector <client> users(num);
-    client userTest;
     std::cout << num << '\n';
+
+    client userTest;
     stream.open(filename, std::ios_base::in | std::ios_base::binary);
     if (!stream.is_open()) {
         std::cerr << "Failed to open file: " << filename << std::endl;
         exit(1);
     }
-    stream.read(reinterpret_cast <char *> (&userTest), num * sizeof(client));
-    std::cout << sizeof(userTest) << ' ' << sizeof(client) << '\n';
-    std::cout << "usrname" << userTest.getPassword();
+    //reading name
+    int nameLen;
+    stream.read(reinterpret_cast<char *> (&nameLen), sizeof(int));
+    std::string name;
+    stream.read(reinterpret_cast<char *> (&name), nameLen);
+
+    userTest.setUsername(name);
+
+    //reading password
+    int passLen;
+    stream.read(reinterpret_cast<char *> (&passLen), sizeof(int));
+    std::string pass;
+    stream.read(reinterpret_cast<char *> (&pass), passLen);
+    userTest.setPassword(pass);
+    //display(userTest);
+
+    // stream.read(reinterpret_cast <char *> (&userTest), num * sizeof(client));
+    // std::cout << sizeof(userTest) << ' ' << sizeof(client) << '\n';
+    // std::cout << "usrname" << userTest.getPassword();
     // for(int i = 0; i < num; i++){
     //     std::cout << userTest.getPassword() << '\n';
     //     std::cout << userTest.getUsername() << '\n';
     // }
 }
 int option;
+
 int main(){
     //setting up filenames and file stream object
     std::fstream stream;
@@ -102,10 +132,9 @@ int main(){
             std::cin.ignore(1, '\n');
 
             //getting user input
-            std::string desiredName;
+            
             std::cout << "Username: ";
             std::cin >> desiredName;
-            std::string desiredPassword;
             std::cout << "Password: ";
             std::cin >> desiredPassword;
 
